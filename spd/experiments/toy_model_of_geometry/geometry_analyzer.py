53310cf57c5e7a55db1ff2f32d79d3451ef24161
Knud Sønderbek <knudsoenderbek@gmail.com>
2026-07-13 19:53:21 +0200

    missing changes?

--- spd/experiments/toy_model_of_geometry/geometry_analyzer.py
+++ spd/experiments/toy_model_of_geometry/geometry_analyzer.py
@@ -426,28 +426,7 @@ def main() -> None:
     dataset = SimplexDataset(ranks, device=device)
 
     run_dirs = [
-        #minimaities
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\minimality_sweep\0.1", "RSPD -Minimality coefficient: 1e-1"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\minimality_sweep\0.01", "RSPD - Minimality coefficient: 1e-2"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\minimality_sweep\0.001", "RSPD - Minimality coefficient: 1e-3"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\minimality_sweep\0.0001", "RSPD - Minimality coefficient: 1e-4"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\minimality_sweep\0.00001", "RSPD - Minimality coefficient: 1e-5"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\minimality_sweep\0.000001", "RSPD - Minimality coefficient: 1e-6"),
-        (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\new_rank_sweep\0.1", "RSPD -Minimality coefficient: 1e-1"),
-        (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\new_rank_sweep\0.01", "RSPD - Minimality coefficient: 1e-2"),
-        (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\new_rank_sweep\0.001", "RSPD - Minimality coefficient: 1e-3"),
-        (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\new_rank_sweep\0.0001", "RSPD - Minimality coefficient: 1e-4"),
-        (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\new_rank_sweep\0.00001", "RSPD - Minimality coefficient: 1e-5"),
-        #(r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\new_rank_sweep\0.000001", "RSPD - Minimality coefficient: 1e-6"),
-        #Rank plots:
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\rank_plots\rank_1", "RSPD - Rank: 1"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\rank_plots\rank_2", "RSPD - Rank: 2"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\rank_plots\rank_3", "RSPD - Rank: 3"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\rank_plots\rank_4", "RSPD - Rank: 4"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\rank_plots\rank_5", "RSPD - Rank: 5"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\rank_plots\rank_6", "RSPD - Rank: 6"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\rank_plots\rank_7", "RSPD - Rank: 7"),
-        # (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\rank_plots\rank_8", "RSPD - Rank: 8"),
+        (r"C:\Users\Knud\uni\spd\spd\experiments\toy_model_of_geometry\out\minimality_sweep\0.1", "RSPD -Minimality coefficient: 1e-1"),
     ]
 
     #Subcomponent level
